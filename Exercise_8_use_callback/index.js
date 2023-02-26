b0cb0d644c35cea89005b73c8c5b144d7e4e53a8
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-26 10:17:59 +0000

    Deleted Exercise_8 copy

--- Exercise_8_use_callback/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.writeFile("textFile.txt", "Hello you! :-)", {encoding: "utf-8"}, (error) => {
-    if(error){
-        console.log(error);
-    } else{
-    console.log(`The file has been successfully created`);
-    }
-});
