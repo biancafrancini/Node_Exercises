020d8d565c59a5e1119342ac8bab022eacd1612a
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-03 17:46:19 +0000

    modified joi validation syntax

--- Exercise_13_Add_Controllers/controllers/planetsController.js
+++ Exercise_13_Add_Controllers/controllers/planetsController.js
@@ -13,7 +13,7 @@ let planets = [
 
 const getAll = (req, res) => {
   //console.log(planets);
-  res.status(200).json(planets);
+  return res.status(200).json(planets);
 };
 
 const getOneById = (req, res) => {
@@ -22,22 +22,26 @@ const getOneById = (req, res) => {
   const planet_by_id = planets.find((planet) => planet.id === Number(id));
   //console.log(planet_by_id);
 
-  res.status(200).json(planet_by_id);
+  return res.status(200).json(planet_by_id);
 };
 
 const create = (req, res) => {
-  const { id, name } = req.body;
-  const schema = Joi.object().keys({
+  const newPlanet = req.body;
+  const schema = Joi.object({
     id: Joi.number().required(),
     name: Joi.string().required(),
   });
 
-  const newPlanet = schema.validate(req.body);
+  const validatedNewPlanet = schema.validate(newPlanet);
 
-  planets = [...planets, newPlanet.value];
+  if(validatedNewPlanet.error){
+    return res.status(400).json({msg: "Some data are not valid"});
+  }
+
+  planets = [...planets, newPlanet];
   //console.log(planets);
 
-  res.status(201).json({ msg: "The new planet has been successfully created" });
+  return res.status(201).json({ msg: "The new planet has been successfully created" });
 };
 
 const updateById = (req, res) => {
@@ -47,7 +51,7 @@ const updateById = (req, res) => {
   planets = planets.map((planet) => planet.id === Number(id) ? { ...planet, name } : planet);
   //console.log(planets);
 
-  res.status(200).json({ msg: "Planet has been successfully updated by ID", planets });
+  return res.status(200).json({ msg: "Planet has been successfully updated by ID", planets });
 };
 
 const deleteById = (req, res) => {
@@ -56,8 +60,10 @@ const deleteById = (req, res) => {
 
   //console.log(planets);
 
-  res.status(200).json({ msg: `Planet with id:${id} was deleted`, planets });
+  return res.status(200).json({ msg: `Planet with id:${id} was deleted`, planets });
 };
+
+
 
 module.exports = {
   getAll,
