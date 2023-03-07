5cec50dbfe4292805a98631809752d4d3ed5de94
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-07 19:00:15 +0000

    added postgres db

---
+++ Exercise_14_Postgres_DB/controllers/planetsController.js
@@ -0,0 +1,78 @@
+const Joi = require("joi");
+
+const db = require("../db");
+
+const setUpDatabase = async () => {
+  await db.query(`CREATE DATABASE planets;`);
+}
+
+setUpDatabase();
+
+
+const getAll = async (req, res) => {
+  const planets = await db.query("SELECT * FROM planets;");
+  console.log(planets);
+  return res.status(200).json(planets);
+};
+
+const getOneById = async (req, res) => {
+  const { id } = req.params;
+  const planet_by_id = await db.query("SELECT * FROM planets WHERE id=$1;", Number(id));
+  console.log(planets);
+
+  return res.status(200).json(planet_by_id);
+};
+
+const create = async (req, res) => {
+  const newPlanet = req.body;
+  const schema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+  });
+
+  const validatedNewPlanet = schema.validate(newPlanet);
+
+  if(validatedNewPlanet.error){
+    return res.status(400).json({msg: "Some data are not valid"});
+  }
+
+  /*const planets = await db.many("SELECT * FROM planets;");
+
+  planets = [...planets, newPlanet];
+  //console.log(planets);*/
+
+  return res.status(201).json({ msg: "The new planet has been successfully created" });
+};
+
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  /*const planets = await db.many("SELECT * FROM planets;");
+
+  planets = planets.map((planet) => planet.id === Number(id) ? { ...planet, name } : planet);
+  //console.log(planets);*/
+
+  return res.status(200).json({ msg: "Planet has been successfully updated by ID"});
+};
+
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+
+  /*const planets = await db.query("SELECT * FROM planets;");
+  planets = planets.filter((planet) => planet.id !== Number(id));
+
+  //console.log(planets);*/
+
+  return res.status(200).json({ msg: `Planet with id:${id} was deleted`});
+};
+
+
+
+module.exports = {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+};
--- Exercise_14_Postgres_DB/index.js
+++ Exercise_14_Postgres_DB/index.js
@@ -1,10 +1,64 @@
-const db = require("./db");
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
 
-(async () => {
+const app = express();
+
+/*(async () => {
     try{
-        const result = await db.query("SELECT * FROM demo");
+        const result = await db.query("SELECT * FROM planets");
         console.log(result);
     }catch(err){
         console.log(err);
     }
-})()
+})()*/
+
+const {
+    getAll,
+    getOneById,
+    create,
+    updateById,
+    deleteById,
+  } = require("./controllers/planetsController");
+  
+  const { SERVER_PORT } = process.env;
+  
+  app.use(express.json());
+  
+  app.use(morgan("dev"));
+  
+  /**
+   * @path api/planets
+   */
+  app.get("/api/planets", getAll);
+  
+  /**
+   * @path api/planets/:id
+   */
+  app.get("/api/planets/:id", getOneById);
+  
+  /**
+   * @path api/planets
+   * @request post
+   */
+  app.post("/api/planets", create);
+  
+  /**
+   * @path api/planets/:id
+   * @request put
+   */
+  app.put("/api/planets/:id", updateById);
+  
+  /**
+   * @path api/planets/:id
+   * @request delete
+   */
+  app.delete("/api/planets/:id", deleteById);
+  
+  
+  
+  app.listen(SERVER_PORT, () => {
+    console.log(`The server listen at port ${SERVER_PORT}`);
+  });
+  
+  module.exports = app;
