0385107f1ade23e84802c532fa97d955016d1643
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-10 08:39:08 +0000

    modified create, updateById and deleteById functions

--- Exercise_14_Database_Postgres/controllers/planetsController.js
+++ Exercise_14_Database_Postgres/controllers/planetsController.js
@@ -3,24 +3,18 @@ const pgPromise = require("pg-promise");
 
 const db = pgPromise({})("postgres://biancafrancini:admin92@localhost:5432/biancafrancini");
 
-const setupDb = async() => {
+const setupDb = async () => {
   await db.none(`
   DROP TABLE IF EXISTS planets;
 
   CREATE TABLE planets (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
-  );`
-  )
+  );`);
 
-  await db.none(
-    `INSERT INTO planets (name) VALUES ('Earth');`
-  )
-  await db.none(
-    `INSERT INTO planets (name) VALUES ('Mars');`
-  )
-
-}
+  await db.none(`INSERT INTO planets (name) VALUES ('Earth');`);
+  await db.none(`INSERT INTO planets (name) VALUES ('Mars');`);
+};
 
 setupDb();
 
@@ -46,34 +40,33 @@ const create = async (req, res) => {
 
   const validatedNewPlanet = schema.validate(newPlanet);
 
-  if(validatedNewPlanet.error){
-    return res.status(400).json({msg: "Some data are not valid"});
-  } else{
-    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
-    return res.status(201).json({ msg: "The new planet has been successfully created" });
+  if (validatedNewPlanet.error) {
+    return res.status(400).json({ msg: "Some data are not valid" });
   }
+  
+  await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+  
+  return res.status(201).json({ msg: "The new planet has been successfully created" });
 };
 
-const updateById = (req, res) => {
+const updateById = async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
-  //planets = planets.map((planet) => planet.id === Number(id) ? { ...planet, name } : planet);
-  //console.log(planets);
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
 
-  return res.status(200).json({ msg: "Planet has been successfully updated by ID"});
+  return res
+    .status(200)
+    .json({ msg: "Planet has been successfully updated by ID" });
 };
 
-const deleteById = (req, res) => {
+const deleteById = async (req, res) => {
   const { id } = req.params;
-  planets = planets.filter((planet) => planet.id !== Number(id));
-
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
   //console.log(planets);
 
-  return res.status(200).json({ msg: `Planet with id:${id} was deleted`});
+  return res.status(200).json({ msg: `Planet with id:${id} was deleted` });
 };
-
-
 
 module.exports = {
   getAll,
--- Exercise_14_Database_Postgres/ex4.md
+++ Exercise_14_Database_Postgres/ex4.md
@@ -7,7 +7,7 @@
 - Using a `setupDb` function:
   - Create `planets` table.✅
   - Populate the table with two planets (e.g. `'Earth'` and `'Mars'`).✅
-- Connect your app to Postgres using Express (`pg-promise`). [https://github.com/vitaly-t/pg-promise]
+- Connect your app to Postgres using Express (`pg-promise`). [https://github.com/vitaly-t/pg-promise] ✅
 - Replace the dummy DB with the Postgres DB. ✅
 - Rewrite all planets controller functions. They should now work with the DB. (Use the SQL queries below.)
 
@@ -25,30 +25,30 @@
   ``` ✅
 
 - Make sure that all CRUD operations read from and write to Postgres (instead of the dummy db you've been using in previous exercises).
-  - `GET /planets`
+  - `GET /planets` ✅
     - Use this SQL query:
       ```sql
-      SELECT * FROM planets;
+      SELECT * FROM planets; 
       ```
-  - `GET /planets/:id`
+  - `GET /planets/:id` ✅
     - Use this SQL query:
       ```sql
-      SELECT * FROM planets WHERE id=$1;
+      SELECT * FROM planets WHERE id=$1; 
       ```
-    - Make sure that `$1` is `id`.
-  - `POST /planets`
+    - Make sure that `$1` is `id`. 
+  - `POST /planets` ✅
     - Use this SQL query:
       ```sql
       INSERT INTO planets (name) VALUES ($1);
       ```
     - Make sure that `$1` is `name`.
-  - `PUT /planets/:id`
+  - `PUT /planets/:id` ✅
     - Use this SQL query:
       ```sql
       UPDATE planets SET name=$2 WHERE id=$1;
       ```
     - Make sure that `$1` is `id` and `$2` is `name`.
-  - `DELETE /planets/:id`
+  - `DELETE /planets/:id` ✅
     - Use this SQL query:
       ```sql
       DELETE FROM planets WHERE id=$1;
