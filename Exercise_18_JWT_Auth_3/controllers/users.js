dd993ed2ee4830a1cae3ac79c3024a8265f3508b
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-12 08:49:29 +0000

    added all previous js file

---
+++ Exercise_18_JWT_Auth_3/controllers/users.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const { db } = require("../db");
+const jwt = require("jsonwebtoken");
+
+const logIn = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await db.one(`SELECT * FROM users WHERE username=$1;`, username);
+
+  if (user && user.password === password) {
+    const payload = {
+      id: user.id,
+      username,
+    };
+
+    const { SECRET = "" } = process.env;
+
+    const token = jwt.sign(payload, SECRET);
+
+    await db.none(`UPDATE users SET token=$2 WHERE id=$1`, [user.id, token]);
+
+    return res.status(200).json({ id: user.id, username, token });
+  } else {
+    return res.status(400).json({ msg: "Username or password not valid" });
+  }
+};
+
+const signUp = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await db.oneOrNone(`SELECT * FROM users WHERE username=$1;`, username);
+
+  if(user){
+    
+    return res.status(409).json({msg: "Signup successful. Now you can log in"});
+  
+} else {
+    
+    const { id } = await db.one(`INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id;`, [username, password]);
+
+    return res.status(201).json({id, msg: "A new user has been successfully created"});
+  }
+};
+
+module.exports = { logIn, signUp };
---
+++ Exercise_18_JWT_Auth_3/index.js
@@ -0,0 +1,91 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const multer = require("multer");
+const app = express();
+
+const {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  uploadImage,
+} = require("./controllers/planetsController");
+
+const { 
+  logIn,
+  signUp,
+ } = require("./controllers/users");
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+})
+
+const upload = multer({ storage });
+
+const { SERVER_PORT } = process.env;
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+
+/**
+ * @path api/planets
+ */
+app.get("/api/planets", getAll);
+
+/**
+ * @path api/planets/:id
+ */
+app.get("/api/planets/:id", getOneById);
+
+/**
+ * @path api/planets
+ * @request post
+ */
+app.post("/api/planets", create);
+
+/**
+ * @path api/planets/:id
+ * @request put
+ */
+app.put("/api/planets/:id", updateById);
+
+/**
+ * @path api/planets/:id
+ * @request delete
+ */
+app.delete("/api/planets/:id", deleteById);
+
+/**
+ * @path api/planets/:id/image
+ * @request post
+ */
+app.post("/api/planets/:id/image", upload.single("image"), uploadImage);
+
+/**
+ * @path api/users/login
+ * @request post
+ */
+app.post("/api/users/login", logIn);
+
+/**
+ * @path api/users/signup
+ * @request post
+ */
+app.post("/api/users/signup", signUp);
+
+
+app.listen(SERVER_PORT, () => {
+  console.log(`The server listen at port ${SERVER_PORT}`);
+});
+
+module.exports = app;
